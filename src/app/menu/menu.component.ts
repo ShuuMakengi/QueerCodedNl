1b35cac85f6930ec64c37ee7f12e491aabb0c5ad
ShuuMakengi <oroszpetra11@gmail.com>
2025-02-19 12:43:55 +0100

    QC-004 Homepage with animation

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
+  imports: [MatButtonModule, MatBadgeModule]
 })
 export class MenuComponent {
 
